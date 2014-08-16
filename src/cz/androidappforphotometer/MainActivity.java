16d690a38af19c3fca0ccf766e830fef40182ec5
Martin Kubík <kubik@outlook.com>
2014-08-16 18:12:19 +0200

    Pridany layout XML soubory.

--- src/cz/androidappforphotometer/MainActivity.java
+++ src/cz/androidappforphotometer/MainActivity.java
@@ -1,9 +1,15 @@
 package cz.androidappforphotometer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +17,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		 setContentView(R.layout.activity_main);
+
+	        ((Button) findViewById(R.id.btn_simple)).setOnClickListener(new OnClickListener() {
+	            @Override
+	            public void onClick(View v) {
+	                startGraphActivity(SimpleGraph.class);
+	            }
+	        });
+	        
+	        ((Button) findViewById(R.id.btn_realtime)).setOnClickListener(new OnClickListener() {
+	            @Override
+	            public void onClick(View v) {
+	                startGraphActivity(RealtimeGraph.class);
+	            }
+	        });
+		
 	}
-}
+	
+	 private void startGraphActivity(Class<? extends Activity> activity) {
+	        Intent intent = new Intent(MainActivity.this, activity);
+	        
+	            intent.putExtra("type", "line");
+	        
+	        startActivity(intent);
+	    }
+	}
